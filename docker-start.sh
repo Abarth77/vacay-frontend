89c97d375c50f43778c2ca3966816dfd36d14e2d
Christopher Lassen <abarth@Christophers-MacBook-Pro.local>
2022-06-17 13:24:59 +0200

    Added improvements to use of docker

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+imageExists="$(docker inspect --type=image vacay-frontend)"
+
+if [[ $imageExists == "" ]]; 
+then
+  docker build -t vacay-frontend .
+else
+    echo "Skipping build, image already exists"
+fi
+
 docker run -p 8080:8080 -it -v $(pwd):/var/www/frontend vacay-frontend
